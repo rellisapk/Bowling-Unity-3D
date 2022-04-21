c55457b7dd17b8f770d13b6e7f2ea4512e2c6502
rellisapk <rllskusuma@gmail.com>
2022-04-21 12:20:57 +0700

    Menu Udah Jadi

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Play(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void HowToPlay(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Skins(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Back(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Exit(){
+        Application.Quit();
+    }
+}
