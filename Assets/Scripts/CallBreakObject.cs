89247b176d062e1a8cc2f3560301dee7cbd49d87
Saddam Habibi Utomo <61404078+saddamsevena@users.noreply.github.com>
2022-05-05 21:09:46 +0700

    Masukin script + tambah bowling lane

---
+++ Assets/Scripts/CallBreakObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CallBreakObject : MonoBehaviour
+{
+    [HideInInspector]
+    public BreakableObject pumpkin;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        pumpkin = GameObject.FindGameObjectWithTag("Pumpkin").GetComponent<BreakableObject>();
+    }
+
+    // Update is called once per frame
+    public void OnTrggerEnter(Collider other)
+    {
+        try
+        {
+            pumpkin.triggerBreak();
+        }
+        catch { }
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public ScoreKeeper _ScoreKeeper;
+    public Text frame;
+    public Text ball;
+    public Text score;
+    public Text down;
+    
+    void Update()
+    {
+        frame.text = "Frame: " + _ScoreKeeper._Frame.ToString();
+        ball.text = "Ball: " + _ScoreKeeper._FrameBall.ToString();
+        score.text = "Score: " + _ScoreKeeper._Score.ToString();
+        down.text = "Down: " + _ScoreKeeper._Down.ToString();
+    }
+}
