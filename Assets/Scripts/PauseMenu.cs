b7f0cb4549b3fc715272c3c84141d021e1822caa
Anastasia Victoria <victoriatasia@gmail.com>
2022-05-10 18:03:30 +0700

    menu pause

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    [SerializeField] GameObject pauseMenu;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
