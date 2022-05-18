3e43d62f5fb5f9d536a969203001630268eb3a15
Anastasia Victoria <victoriatasia@gmail.com>
2022-05-18 07:50:13 +0700

    bola gerak

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody rb;
+    [SerializeField]
+    float power;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+            rb.AddForce(Vector3.forward * power);
+    }
+}
