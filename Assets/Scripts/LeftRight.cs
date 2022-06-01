565b8f6bbbf98fce6fd4daf090b86762f666c3e8
Anastasia Victoria <victoriatasia@gmail.com>
2022-06-01 09:12:51 +0700

    kiri kanan

---
+++ Assets/Scripts/LeftRight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftRight : MonoBehaviour
+{
+    public Rigidbody rb;
+    [SerializeField]
+    float power;
+    public Vector3 posisiAwal;
+
+    public void MoveLeft()
+    {
+        rb.AddForce(Vector3.left * power * Time.deltaTime);
+    }
+
+    public void MoveRight()
+    {
+        rb.AddForce(Vector3.right * power * Time.deltaTime);
+    }
+}
